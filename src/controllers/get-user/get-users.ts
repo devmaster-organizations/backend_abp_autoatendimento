6aa94f5bfa40cf24993c5f1ffeb8c82615a4f7df
Victor Ramos <v.ramos58@hotmail.com>
2026-03-26 14:30:02 -0300

    feat: conectando banco ao repository

--- src/controllers/get-user/get-users.ts
+++ src/controllers/get-user/get-users.ts
@@ -3,6 +3,7 @@ import type { IGetUsersController, IGetUsersRepository } from './protocols';
 export class GetUsersController implements IGetUsersController {
     constructor(private readonly getUsersRepository: IGetUsersRepository) {}
     async handler() {
+        console.log("Handling request to get users...");
         const users = await this.getUsersRepository.getUsers();
         return {
             statusCode: 200,
--- src/repositories/get-users/postgres-get-users.ts
+++ src/repositories/get-users/postgres-get-users.ts
@@ -1,20 +1,11 @@
 import type { IGetUsersRepository } from "../../controllers/get-user/protocols";
+import { prisma } from "../../core/database/prisma";
 
 export class PostgresGetUsers implements IGetUsersRepository {
     async getUsers() {
-        return [
-            {
-                id: 2,
-                name: 'John Doe',
-                email: 'john.doe@example.com',
-                password: 'password123'
-            },
-            {
-                id: 3,
-                name: 'Jane Doe',
-                email: 'jane.doe@example.com',
-                password: 'password456'
-            },
-        ];
+        console.log("Getting users from PostgreSQL database...");
+        // chamar usuários
+        const users = await prisma.user.findMany();
+        return users;
     }
 }
